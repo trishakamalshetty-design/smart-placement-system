11f5d95df969a293cd8e0c512090dff0df44fa11
TrishaKamalShetty <TrishaKamalShetty@gmail.com>
2026-04-11 23:25:32 +0530

    Use MongoDB Atlas URL

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 // MongoDB Connection
 mongoose
-  mongoose.connect("mongodb://127.0.0.1:27017/collegeDB")
+  mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
