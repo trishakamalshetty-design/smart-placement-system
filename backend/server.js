4b5dbd11716a78bd248978c152530da5b856ea4e
TrishaKamalShetty <TrishaKamalShetty@gmail.com>
2026-04-12 13:17:01 +0530

    fix server connection

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 
 const app = express();
 
+// PORT
+const PORT = process.env.PORT || 5000;
+
 // Middleware
 app.use(cors({
   origin: "http://localhost:5173",
@@ -13,19 +16,22 @@ app.use(cors({
 
 app.use(express.json());
 
-// MongoDB Connection
-mongoose
-  mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
-
 // Routes
 app.use('/api/students', require('./routes/studentRoutes'));
 app.use('/api/companies', require('./routes/companyRoutes'));
 app.use('/api/placements', require('./routes/placementRoutes'));
 app.use('/api/reports', require('./routes/reportRoutes'));
 
-// Server start
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+// MongoDB Connection + Server Start (ONLY ONCE)
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB Connected");
+
+    app.listen(PORT, () => {
+      console.log("Server running on port", PORT);
+    });
+
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Error:", err);
+  });
